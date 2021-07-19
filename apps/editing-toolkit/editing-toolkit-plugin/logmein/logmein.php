653838893a976d1c8f4feaaaabf42732ea35c54f
Louis Laugesen <louis.laugesen@gmail.com>
2021-07-19 17:13:10 +1000

    Php cleanup

--- apps/editing-toolkit/editing-toolkit-plugin/logmein/logmein.php
+++ apps/editing-toolkit/editing-toolkit-plugin/logmein/logmein.php
@@ -11,11 +11,7 @@ namespace A8C\FSE\Logmein;
  * Init
  */
 function logmein_init() {
-	static $once = false;
-	if ( ! $once ) {
-		$once = true;
-		add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\logmein_enqueue', 100 );
-	}
+	add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\logmein_enqueue', 100 );
 }
 add_action( 'init', __NAMESPACE__ . '\logmein_init' );
 
@@ -23,40 +19,15 @@ add_action( 'init', __NAMESPACE__ . '\logmein_init' );
  * Enqueue
  */
 function logmein_enqueue() {
-	$asset_file          = include __DIR__ . '/dist/logmein.asset.php';
-	$script_dependencies = $asset_file['dependencies'];
-	$version             = $asset_file['version'];
+	$assets       = include __DIR__ . '/dist/logmein.asset.php';
+	$dependencies = isset( $assets['dependencies'] ) ? $assets['dependencies'] : array();
+	$version      = isset( $assets['version'] ) ? $assets['version'] : filemtime( __DIR__ . '/index.ts' );
 
 	wp_enqueue_script(
 		'wpcom-logmein',
 		plugins_url( 'dist/logmein.js', __FILE__ ),
-		is_array( $script_dependencies ) ? $script_dependencies : array(),
+		$dependencies,
 		$version,
 		true
 	);
-}
-
-/**
- * Api
- */
-function logmein_api() {
-	register_rest_route(
-		'wpcom/v2',
-		'logmein',
-		array(
-			array(
-				'methods'  => \WP_REST_Server::READABLE,
-				'callback' => __NAMESPACE__ . '\\logmein_hosts',
-			),
-			'permission_callback' => '\is_user_logged_in',
-		)
-	);
-}
-
-/**
- * Hosts
- */
-function logmein_hosts() {
-	$hosts = apply_filters( 'wpcom_logmein_hosts', array() );
-	return rest_ensure_response( compact( 'hosts' ) );
 }
