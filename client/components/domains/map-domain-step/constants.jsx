05bb0b867f52f0c65ddeab209595ed8621661b46
Del Putnam <del@putnams.net>
2021-07-20 17:29:53 -0400

    Add new domain mapping steps

---
+++ client/components/domains/map-domain-step/constants.jsx
@@ -0,0 +1,10 @@
+export const modeType = {
+	SUGGESTED: 'suggested',
+	ADVANCED: 'advanced',
+};
+
+export const stepType = {
+	START: 'start_setup',
+	LOG_IN_TO_PROVIDER: 'log_in_to_provider',
+	UPDATE_DNS_RECORDS: 'update_dns_records',
+};
---
+++ client/components/domains/map-domain-step/map-domain-steps-suggested.jsx
@@ -0,0 +1,57 @@
+/**
+ * External dependencies
+ */
+
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { __, sprintf } from '@wordpress/i18n';
+import { createElement, createInterpolateElement } from '@wordpress/element';
+import { Card } from '@automattic/components';
+
+/**
+ * Internal dependencies
+ */
+import { modeType, stepType } from './constants';
+import CardHeading from 'calypso/components/card-heading';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+export default function MapDomainStepsSuggested( {
+	className,
+	domain,
+	step,
+	onChangeStep,
+	onChangeMode,
+} ) {
+	const setModeSuggested = () => onChangeMode( modeType.ADVANCED );
+
+	return (
+		<Card className={ className }>
+			<CardHeading className="map-domain-step__heading">Suggested setup</CardHeading>
+			<p>
+				{ createInterpolateElement(
+					__(
+						'This is the easiest way to connect your domain, using name servers. If needed you can also use our <a>advanced setup</a>, using root A & CNAME records.'
+					),
+					{
+						a: createElement( 'a', {
+							className: 'map-domain-step__change_mode_link',
+							onClick: setModeSuggested,
+						} ),
+					}
+				) }
+			</p>
+		</Card>
+	);
+}
+
+MapDomainStepsSuggested.propTypes = {
+	className: PropTypes.string,
+	domain: PropTypes.string.isRequired,
+	step: PropTypes.oneOf( Object.values( stepType ) ).isRequired,
+	onChangeStep: PropTypes.func.isRequired,
+	onChangeMode: PropTypes.func.isRequired,
+};
