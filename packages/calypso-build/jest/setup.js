77fbedfd8b078d1106fdd2c665d90eec9a087714
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-22 14:37:46 +0200

    Update enzyme adapter

--- packages/calypso-build/jest/setup.js
+++ packages/calypso-build/jest/setup.js
@@ -2,6 +2,6 @@
  * External dependencies
  */
 const { configure } = require( 'enzyme' );
-const Adapter = require( 'enzyme-adapter-react-16' );
+const Adapter = require( '@wojtekmaj/enzyme-adapter-react-17' );
 
 configure( { adapter: new Adapter() } );
